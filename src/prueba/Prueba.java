185fe1212835ce48cb1517c556ce78461ae62ee9
LorenzoLongaretto <lorenzolongarettolok@gmail.com>
2021-05-05 19:48:50 -0300

    1

--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -15,10 +15,30 @@ public class Prueba {
 		
 		IMedico medico = MedicoFactory.getMedico("25900987","Luis","Montini","MDP","2234565","Independencia","1234","Cirujia","Permanente","Magister");
 		
-        //Paciente paciente  = PacienteFactory.getPaciente(null, null, null, null, null, null, null, null, 0);
-	
-        //Clinica.getInstance().derivarPaciente(paciente);
+        Paciente paciente  = PacienteFactory.getPaciente("321", "fa", "lop", "fds","fsdf", "fsdfs", "", "Nino", 0);
+        Paciente paciente2  = PacienteFactory.getPaciente("321", "lop", "fa", "fds","fsdf", "fsdfs", "", "Mayor", 0);
+        Paciente paciente3  = PacienteFactory.getPaciente("321", "32", "lop", "fds","fsdf", "fsdfs", "", "Nino", 0);
+        Paciente paciente4  = PacienteFactory.getPaciente("321", "negro", "blanco", "fds","fsdf", "fsdfs", "", "Joven", 0);
+       
         System.out.println(medico.getHonorario());
+        System.out.println(medico);
+        Clinica.getInstance().derivarPaciente(paciente);
+        Clinica.getInstance().derivarPaciente(paciente2);
+        Clinica.getInstance().derivarPaciente(paciente3);
+        Clinica.getInstance().derivarPaciente(paciente4);//double dispach 10/10
+        System.out.println(Clinica.getInstance().getSalaEspera().getPaciente());
+        System.out.println("LOS PACIENTES QUE ESTAN EN EL PATIO   "+Clinica.getInstance().getPatio());
+        Clinica.getInstance().atenderPaciente(paciente);
+        
+        System.out.println(paciente);
+        /*
+         * falta 
+         * hacer las historias clinicas
+         * implementar la facturacion
+         * excepciones
+         * arrancar la segunda parte de la mv
+         * 
+         */
 	}
  
 }
