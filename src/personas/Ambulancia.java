4e90214d2a58b60865b78c29ba0d1ad38f5a9a41
LorenzoLongaretto <lorenzolongarettolok@gmail.com>
2021-06-22 17:28:40 -0300

    a

--- src/personas/Ambulancia.java
+++ src/personas/Ambulancia.java
@@ -7,20 +7,22 @@ import util.Util;
 public class Ambulancia extends Observable{
 	private static Ambulancia instancia = null;
 	private IState estado;
-	private boolean disponible,regresandoSinP; //regeresando sin paciente
+	private boolean disponible,trasladandoP,atendiendoDom,regresandoSinP,enTaller,regresandoTaller; //regeresando sin paciente
 	
 	public Ambulancia() {
 		this.estado = new DisponibleState(this);
 		this.disponible=true;
 		this.regresandoSinP=false;
+		this.enTaller=false;
+		this.regresandoTaller=false;
+		this.trasladandoP=false;
+		this.atendiendoDom=false;
 	}
 	public static Ambulancia getInstancia() {
 		if (instancia == null)
 			instancia = new Ambulancia();
 		return instancia;
 	}
-	
-	
 	
 	
 	public IState getEstado() {
@@ -32,6 +34,8 @@ public class Ambulancia extends Observable{
 	}
 
 	public synchronized void solicitaAtencion(){ //a domicilio
+		System.out.println("estimulo de atencion");
+		
 		while ( !(this.disponible || this.regresandoSinP)) {
 			try {
 				System.out.println("solicita atencion y no puede");
@@ -42,16 +46,20 @@ public class Ambulancia extends Observable{
 			
 		}
 		this.disponible=false;
-		//System.out.println(this.estado.actual());
+		this.regresandoSinP=false;
+		this.atendiendoDom=true;
 		
+		System.out.println("estaba  "+this.estado.actual());
 		this.estado.solicitaAtencion();
-		System.out.println(this.estado.actual());
+		System.out.println("ahora esta "+this.estado.actual());
 		this.notifyObservers(this.estado.actual());
 		notifyAll();
+		System.out.println("-------------");
 		
 	}
 	
 	public synchronized void solicitaTraslado(){
+		System.out.println("estimulo de traslado");
 		while(!this.disponible) {
 			try {
 				System.out.println("solicita traslado y no puede");
@@ -63,28 +71,18 @@ public class Ambulancia extends Observable{
 		this.regresandoSinP=true;
 		this.disponible=false;
 		
-		//System.out.println(this.estado.actual());
+		
+		System.out.println("estaba "+this.estado.actual());
 		this.estado.solicitaTraslado();
-		System.out.println(this.estado.actual());
+		System.out.println("ahora esta "+this.estado.actual());
 		this.notifyObservers(this.estado.actual());
 		notifyAll();
-		
+		System.out.println("-------------");
 		
 	}
 	
-	public synchronized void volverClinica(){
-		
-		this.estado.volverClinica();
-		System.out.println(this.estado.actual());
-		this.notifyObservers(this.estado.actual());
-		this.disponible=true;
-		this.regresandoSinP=false;
-		notifyAll();
-		
-	}
-	
-	public synchronized void solicitaReparacion(){
-		
+		public synchronized void solicitaReparacion(){
+		System.out.println("estimulo de reparacion");
 		while(!this.disponible)
 			try {
 				System.out.println("solicita reparacion y no puede");
@@ -93,14 +91,60 @@ public class Ambulancia extends Observable{
 				e.printStackTrace();
 			}
 		this.disponible=false;
+		this.enTaller=true;
 		
-		
+		System.out.println("estaba"+this.estado.actual());
 		this.estado.solicitaReparacion();
-		System.out.println(this.estado.actual());
+		System.out.println("ahora esta "+this.estado.actual());
 		this.notifyObservers(this.estado.actual());
 		notifyAll();
+		System.out.println("-------------");
 	
 	}
+		
+	public synchronized void volverClinica(){
+		System.out.println("estaba "+this.estado.actual());
+		this.estado.volverClinica();
+		System.out.println("ahora esta "+this.estado.actual());
+		this.notifyObservers(this.estado.actual());
+		
+		/*while (this.disponible) {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}*/
+		
+		if(this.enTaller) {
+			this.regresandoTaller=true;
+			this.enTaller=false;
+			this.volverClinica();
+		}
+		else if(this.regresandoTaller) {
+			this.disponible=true;
+			this.regresandoTaller=false;
+		}
+		else if(this.regresandoSinP) {
+			this.regresandoSinP=false;
+			this.disponible=true;
+		}else if(this.atendiendoDom) {
+			this.atendiendoDom=false;
+			this.trasladandoP=true;
+			this.volverClinica();
+			
+		}
+		else if(this.trasladandoP) {
+			this.trasladandoP=false;
+			this.disponible=true;
+		}
+			
+		notifyAll();
+		System.out.println("-------------");
+		
+	}
+	
+
 
 	
 
